4cd18bedc12f6a05181a74455b6f88d4318af9c2
DESKTOP-7JVI354\Yahya <yahyaejaz075@gmail.com>
2021-03-17 04:33:44 -0700

    all files push to github

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,83 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Variables and DataTypes
+		
+//		String a = "My name is Yahya";
+//		System.out.println(a);
+		
+		// Arithmetic operators +,-.*,/
+		
+//		float a = 20f;
+//		float b = 0f;
+//		
+//		System.out.println(a / b);
+		
+		// relational operators <,>,==,!=
+		
+//		int c = 10;
+//		int d = 20;
+//		System.out.println(c != d);
+		
+		// logical operators &,|
+		
+//		boolean a = true;
+//		boolean b = false;
+//		
+//		System.out.println(a | b);
+		
+	// if else statements
+		
+//		int marks = 33;
+//		if(marks < 50)
+//		{
+//			System.out.println("Fail");
+//		}
+//		else if(marks>=50 && marks<60)
+//		{
+//			System.out.println("D grade");
+//		}
+//		
+//		else if(marks>=60 && marks < 75)
+//		{
+//			System.out.println("C grade");
+//		}
+//		
+//		else if(marks>=75 && marks < 90)
+//		{
+//			System.out.println("B grade");
+//		}
+//		
+//		else if(marks>= 90 && marks <= 100)
+//		{
+//			System.out.println("A+ grade");
+//		}
+//		
+//		else
+//		{
+//			System.out.println("Invalid");
+//		}
+		
+		int number = 0;
+		if(number > 0)
+		{
+			System.out.println("Number is positive");
+		}
+		
+		else if(number < 0)
+		{
+			System.out.println("Number is negative");
+		}
+		
+		else
+		{
+			System.out.println("Number is zero");
+		}
+		
+
+	}
+
+}
